7e1715796c0855ebc40b3bcf6d88821becf74e07
İrem Kabaoğlu <iremkabaoglu1@gmail.com>
2025-08-06 16:22:25 +0300

    Initial commit - MovixApp proje yüklemesi

---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovixApp.Models
+{
+    public class Movie
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("overview")]
+        public string Overview { get; set; }
+
+        [JsonPropertyName("poster_path")]
+        public string PosterPath { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public double VoteAverage { get; set; }
+    }
+
+    public class MovieResponse
+    {
+        [JsonPropertyName("results")]
+        public List<Movie> Results { get; set; }
+    }
+}
